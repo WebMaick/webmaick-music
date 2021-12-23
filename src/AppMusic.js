4293f50b54afe7b0934ab5dc9cf5f8db10cd10d9
webmaick <webmaick@gmail.com>
2021-12-22 20:35:26 -0400

    Configuracion Inicial

---
+++ src/AppMusic.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const AppMusic = () => {
+  return (
+    <div>
+      <h2 className='miguel'>Bienvenido WebMaick</h2>
+      <button className='btn btn__primary'>hola prueba</button>
+    </div>
+  )
+}
